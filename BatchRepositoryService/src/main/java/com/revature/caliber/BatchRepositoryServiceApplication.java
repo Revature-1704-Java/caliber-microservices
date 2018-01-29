ba91a1b1be24219eabffa7a97336e0650aca0ef5
Kei Peralta <cnmperalta@gmail.com>
2018-01-29 14:26:42 -0500

    Initial commit with Spring Boot applications for each repository

---
+++ BatchRepositoryService/src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.revature.caliber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchRepositoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchRepositoryServiceApplication.class, args);
+	}
+}
---
+++ PanelRepositoryService/src/main/java/com/revature/caliber/PanelRepositoryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.revature.caliber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelRepositoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanelRepositoryServiceApplication.class, args);
+	}
+}
