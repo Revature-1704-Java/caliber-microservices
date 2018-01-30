fe4894fa4a449824130a1a89bb6a3f5057204f48
Kei Peralta <cnmperalta@gmail.com>
2018-01-30 10:15:07 -0500

    Add special find methods

--- NoteRepositoryService/src/main/java/com/revature/caliber/repository/NoteRepository.java
+++ NoteRepositoryService/src/main/java/com/revature/caliber/repository/NoteRepository.java
@@ -1,11 +1,17 @@
 package com.revature.caliber.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.caliber.model.Note;
+import com.revature.caliber.model.NoteType;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-
+	Note findByTraineeIdAndWeekAndQcFeedbackAndType(Integer traineeId, Short week, boolean qcFeedback, NoteType type);
+	
+	List<Note> findByTraineeIdAndType(Integer traineeId, NoteType type);
+	List<Note> findByBatchIdAndWeekAndQcFeedbackAndType(Integer batchId, Short week, boolean qcFeedback, NoteType type);
 }
