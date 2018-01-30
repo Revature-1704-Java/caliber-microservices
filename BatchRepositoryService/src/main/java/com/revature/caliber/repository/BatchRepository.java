41be2d0eea2b1290b370da9635a53b7d9e576fda
Tzu Chao <chao.tzu5@gmail.com>
2018-01-30 13:01:32 -0500

    add repository annotation

--- BatchRepositoryService/src/main/java/com/revature/caliber/repository/BatchRepository.java
+++ BatchRepositoryService/src/main/java/com/revature/caliber/repository/BatchRepository.java
@@ -3,9 +3,10 @@ package com.revature.caliber.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.caliber.model.Batch;
-
+@Repository
 public interface BatchRepository extends JpaRepository<Batch, Integer> {
 	List <Batch> findAllByTrainerId(Integer trainerId);
 }
