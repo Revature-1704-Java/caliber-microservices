0d7509baf841eca3a878cfef068152f1d3e418cf
Kei Peralta <cnmperalta@gmail.com>
2018-01-30 09:19:33 -0500

    Add Spring Data REST dependency and add test annotations

--- NoteRepositoryService/src/test/java/com/revature/caliber/test/unit/NoteRepositoryTest.java
+++ NoteRepositoryService/src/test/java/com/revature/caliber/test/unit/NoteRepositoryTest.java
@@ -1,15 +1,15 @@
 package com.revature.caliber.test.unit;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.revature.caliber.model.Note;
-import com.revature.caliber.model.NoteType;
-import com.revature.caliber.model.QCStatus;
 import com.revature.caliber.repository.NoteRepository;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class NoteRepositoryTest {
 	@Autowired
 	private NoteRepository noteRepository;
