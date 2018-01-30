cfc6af9ef24e1be6d0fcd5d9b2d26d74e72eba00
Kei Peralta <cnmperalta@gmail.com>
2018-01-30 09:30:30 -0500

    Finish configuration for H2 database

--- NoteRepositoryService/src/test/java/com/revature/caliber/test/unit/NoteRepositoryTest.java
+++ NoteRepositoryService/src/test/java/com/revature/caliber/test/unit/NoteRepositoryTest.java
@@ -1,11 +1,20 @@
 package com.revature.caliber.test.unit;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.caliber.model.Note;
+import com.revature.caliber.model.NoteType;
+import com.revature.caliber.model.QCStatus;
 import com.revature.caliber.repository.NoteRepository;
 
 @RunWith(SpringRunner.class)
@@ -16,13 +25,24 @@ public class NoteRepositoryTest {
 	
 	@Test
 	public void findOneTest() {
-//		Note note = noteRepository.findOne(6482);
-//		short week = 6;
-//		
-//		assertEquals("Good", note.getContent());
-//		assertEquals(NoteType.QC_TRAINEE, note.getType());
-//		assertEquals(QCStatus.Good, note.getQcStatus());
-//		assertEquals(week, note.getWeek().shortValue());
+		Note note = noteRepository.findOne(6482);
+		short week = 6;
+		
+		System.out.println(note);
+		
+		assertNotNull(note);
+		assertEquals(NoteType.QC_TRAINEE, note.getType());
+		assertEquals(QCStatus.Good, note.getQcStatus());
+		assertEquals(week, note.getWeek().shortValue());
+	}
+	
+	@Test
+	public void findAllTest() {
+		List<Note> notes = noteRepository.findAll();
+		
+		System.out.println(notes.size());
+		
+		assertFalse(notes.isEmpty());
 	}
 
 }
