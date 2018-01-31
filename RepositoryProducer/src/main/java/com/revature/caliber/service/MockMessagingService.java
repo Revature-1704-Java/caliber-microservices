9510cb67e427a14a7f7e493e2019e993f0daa4bd
Kei Peralta <cnmperalta@gmail.com>
2018-01-31 15:27:29 -0500

    Use JSON for microservice communication

--- RepositoryProducer/src/main/java/com/revature/caliber/service/MockMessagingService.java
+++ RepositoryProducer/src/main/java/com/revature/caliber/service/MockMessagingService.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.JsonObject;
+import com.revature.caliber.model.SimpleNote;
+
 @Service
 public class MockMessagingService {
 
@@ -11,8 +14,15 @@ public class MockMessagingService {
 	AmqpTemplate rabbitTemplate;
 	
 	public boolean send(String routingKey, String message) {
-		String response = (String) rabbitTemplate.convertSendAndReceive("revature.caliber.repos", routingKey, message);
+		JsonObject object = new JsonObject();
+		
+		object.addProperty("methodName", "findTraineeNote");
+		object.addProperty("traineeId", 5529);
+		object.addProperty("week", 2);
+		
+		SimpleNote response = (SimpleNote) rabbitTemplate.convertSendAndReceive("revature.caliber.repos", routingKey, object.toString());
 		System.out.println(response);
+		
 		return true;
 	}
 }
