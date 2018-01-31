a59ede14d21aaca3317077aa2b979111af6edd31
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-01-31 10:33:34 -0500

    Conversion to @RepositoryRestResource complete

--- PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/repository/PanelFeedbackRepository.java
+++ PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/repository/PanelFeedbackRepository.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.revature.caliber.model.SimplePanelFeedback;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "panelfeedback", path = "panelfeedback")
 public interface PanelFeedbackRepository extends JpaRepository<SimplePanelFeedback, Long> {
 
-	@Query("SELECT DISTINCT pf FROM PanelFeedback pf")
+	@Query("SELECT DISTINCT pf FROM SimplePanelFeedback pf")
 	List<SimplePanelFeedback> findAll();
 	
-	@Query("SELECT DISTINCT pf FROM PanelFeedback pf WHERE pf.panelId= ?1")
-	List<SimplePanelFeedback> findAllForPanel(int panelId);
+	@Query("SELECT DISTINCT pf FROM SimplePanelFeedback pf WHERE pf.panelId= ?1")
+	List<SimplePanelFeedback> findAllForPanel(@Param("panelId") Integer panelId);
 	
-	@Query("SELECT DISTINCT pf FROM PanelFeedback pf WHERE pf.panelId= ?1 and pf.status='Repanel'")
-	List<SimplePanelFeedback> findFailedFeedbackByPanel(int panelId);
+	@Query("SELECT DISTINCT pf FROM SimplePanelFeedback pf WHERE pf.panelId= ?1 and pf.status='Repanel'")
+	List<SimplePanelFeedback> findFailedFeedbackByPanel(@Param("panelId") Integer panelId);
 }
