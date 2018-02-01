2dda6a2d9c20ee4fce4d2abcc4a825e99c31ed8e
Kei Peralta <cnmperalta@gmail.com>
2018-02-01 14:27:05 -0500

    Start adding composition to repository

--- NoteCompositionService/src/main/java/com/revature/caliber/service/NoteCompositionMessagingService.java
+++ NoteCompositionService/src/main/java/com/revature/caliber/service/NoteCompositionMessagingService.java
@@ -1,13 +1,26 @@
 package com.revature.caliber.service;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteCompositionMessagingService {
-	@RabbitListener(queues = "caliber.note")
-	public String receive(String message) {
-		System.out.println(message);
-		return "NoteCompositionMessagingService is ready";
+	
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+	
+	private static final String SINGLE_NOTE_ROUTING_KEY = "JBPpcrjZn59EGH4k";
+	private static final String LIST_NOTE_ROUTING_KEY = "cf22J9CGs8V95Rbm";
+	
+	public boolean sendSingleNoteRequest() {
+		
+		return true;
+	}
+	
+	public boolean sendListNoteRequest() {
+		
+		return true;
 	}
 }
