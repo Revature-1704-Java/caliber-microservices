6daed71e4fdfb1dd117d39706fca932c28a14159
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-02-02 16:29:12 -0500

    PanelFeedback done

--- PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/config/PanelFeedbackRepositoryServiceConfiguration.java
+++ PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/config/PanelFeedbackRepositoryServiceConfiguration.java
@@ -9,37 +9,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.gson.JsonObject;
+import com.revature.caliber.service.PanelFeedbackCompositionService;
 import com.revature.caliber.service.PanelFeedbackRepositoryMessagingService;
 
 @Configuration
-public class RepositoryProducerConfiguration {
+public class PanelFeedbackRepositoryServiceConfiguration {
 
 	@Autowired
-	private PanelFeedbackRepositoryMessagingService mms;
+	PanelFeedbackCompositionService panelFeedbackCompositionService;
 	
 	@Bean
 	public AmqpTemplate rabbitTemplate(ConnectionFactory factory) {
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(factory);
-		rabbitTemplate.setExchange("revature.caliber.repos");
 		return new RabbitTemplate(factory);
 	}
 	
-	/*
 	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			//findOne test
-			JsonObject object = new JsonObject();
-			object.addProperty("methodName", "findOne");
-			object.addProperty("panelFeedbackId", 1);
-			
-			//findAll test
-			JsonObject object2 = new JsonObject();
-			object2.addProperty("methodName", "findAll");
-			
-			mms.send("4jZ2GMxLP7VyQPBn", object.toString()); //single 
-			//mms.send("5MKVoktka2jXh9yR", object.toString());              //Lists
-		};
+	public PanelFeedbackCompositionService panelFeedbackCompositionService() {
+		return new PanelFeedbackCompositionService();
 	}
-	*/
 }
