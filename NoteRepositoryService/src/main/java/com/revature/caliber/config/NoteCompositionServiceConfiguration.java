90139dd105c418281423f67c4d2f4a8d60e06e30
Kei Peralta <cnmperalta@gmail.com>
2018-02-02 23:20:21 -0500

    Remove unnecessary imports

--- NoteRepositoryService/src/main/java/com/revature/caliber/config/NoteCompositionServiceConfiguration.java
+++ NoteRepositoryService/src/main/java/com/revature/caliber/config/NoteCompositionServiceConfiguration.java
@@ -3,12 +3,9 @@ package com.revature.caliber.config;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.revature.caliber.model.Note;
 import com.revature.caliber.service.NoteCompositionService;
 
 @Configuration
