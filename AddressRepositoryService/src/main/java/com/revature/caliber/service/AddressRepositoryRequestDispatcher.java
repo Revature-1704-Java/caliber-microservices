52eab122fc203b80032e78ec45bc95e021dbf1b9
Kei Peralta <cnmperalta@gmail.com>
2018-02-05 10:38:09 -0500

    Add Address Repository functionality

---
+++ AddressRepositoryService/src/main/java/com/revature/caliber/service/AddressRepositoryRequestDispatcher.java
@@ -0,0 +1,38 @@
+package com.revature.caliber.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.gson.JsonObject;
+import com.revature.caliber.model.Address;
+import com.revature.caliber.repository.AddressRepository;
+
+public class AddressRepositoryRequestDispatcher {
+	@Autowired
+	private AddressRepository addressRepository;
+	
+	public Address processSingleAddressRequest(JsonObject request) {
+		Address result = null;
+		String methodName = request.get("methodName").getAsString();
+		
+		if(methodName.equals("findOne")) {
+			int addressId = request.get("addressId").getAsInt();
+			result = addressRepository.findOne(addressId);
+		}
+		
+		return result;
+	}
+	
+	public List<Address> processListAddressRequest(JsonObject request) {
+		List<Address> result = null;
+		String methodName = request.get("methodName").getAsString();
+		
+		if(methodName.equals("findAll")) {
+			result = addressRepository.findAll();
+		}
+		
+		return result;
+	}
+	
+}
