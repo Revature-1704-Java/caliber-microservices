db0514518eab8108ce984c550f619abcf628177f
Daniel <djt52@case.edu>
2018-02-06 14:59:50 -0500

    Added swagger to Batch and Trainee microservices

--- BatchRepositoryService/src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
+++ BatchRepositoryService/src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
@@ -8,7 +8,14 @@ import org.springframework.context.annotation.Bean;
 
 import com.revature.caliber.service.BatchCompositionService;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class BatchRepositoryServiceApplication {
 	@Autowired
 	BatchCompositionService bcs;
@@ -21,4 +28,13 @@ public class BatchRepositoryServiceApplication {
 //			System.out.println(bcs.findOne(2100).getTrainees());
 //		};
 //	}
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.revature.caliber.controller"))              
+          .paths(PathSelectors.any())                          
+          .build();
+    }
+	
 }
