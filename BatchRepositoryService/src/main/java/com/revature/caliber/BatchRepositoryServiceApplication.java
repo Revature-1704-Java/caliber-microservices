ff7de122edc0f1da6d3186d14e4a1649fbbf6ad3
Tzu Chao <chao.tzu5@gmail.com>
2018-02-06 15:45:23 -0500

    complete message delete to trainee

--- BatchRepositoryService/src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
+++ BatchRepositoryService/src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
@@ -6,19 +6,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.revature.caliber.model.SimpleBatch;
+import com.revature.caliber.service.BatchCompositionMessageService;
 import com.revature.caliber.service.BatchCompositionService;
 
 @SpringBootApplication
 public class BatchRepositoryServiceApplication {
 	@Autowired
-	BatchCompositionService bcs;
+	BatchCompositionMessageService bmcs;
 	public static void main(String[] args) {
 		SpringApplication.run(BatchRepositoryServiceApplication.class, args);
 	}
 //	@Bean
 //	public CommandLineRunner runner() {
 //		return args -> {
-//			System.out.println(bcs.findOne(2100).getTrainees());
+//			System.out.println("here");
+//			System.out.println(bmcs.sendListSimpleTraineeRequest(2100));
+//			bmcs.sendSimpleTraineeDeleteRequest(2100);
+//			System.out.println(bmcs.sendListSimpleTraineeRequest(2100));
 //		};
 //	}
 }
