c768efe653964c9d44c1c2ca0fca4e2df1bb1a85
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-02-06 15:42:25 -0500

    Old import from cmdRunner Removed

--- PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/PanelFeedbackRepositoryServiceApplication.java
+++ PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/PanelFeedbackRepositoryServiceApplication.java
@@ -1,15 +1,7 @@
 package com.revature.caliber;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.google.gson.JsonObject;
-import com.revature.caliber.service.PanelFeedbackCompositionMessagingService;
-import com.revature.caliber.service.PanelFeedbackCompositionService;
-import com.revature.caliber.service.PanelFeedbackRepositoryMessagingService;
 
 @SpringBootApplication
 public class PanelFeedbackRepositoryServiceApplication {
@@ -17,7 +9,4 @@ public class PanelFeedbackRepositoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PanelFeedbackRepositoryServiceApplication.class, args);
 	}
-	
-	@Autowired
-	PanelFeedbackCompositionService panelFeedbackCompositionService;
 }
