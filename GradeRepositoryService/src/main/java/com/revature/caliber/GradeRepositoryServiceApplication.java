e570d2bd3146e8c3beb87348e6861392a5b402fd
Daniel <djt52@case.edu>
2018-02-07 13:48:46 -0500

    Added Swagger to Trainee and Grade

--- GradeRepositoryService/src/main/java/com/revature/caliber/GradeRepositoryServiceApplication.java
+++ GradeRepositoryService/src/main/java/com/revature/caliber/GradeRepositoryServiceApplication.java
@@ -3,9 +3,17 @@ package com.revature.caliber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.revature.caliber.repository.GradeRepository;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class GradeRepositoryServiceApplication {
 
@@ -13,4 +21,13 @@ public class GradeRepositoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GradeRepositoryServiceApplication.class, args);
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.revature.caliber.controller"))              
+          .paths(PathSelectors.any())                          
+          .build();
+    }
 }
