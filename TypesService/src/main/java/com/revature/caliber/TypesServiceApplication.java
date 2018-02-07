5f6396d22afd84ec975ca50467b147bce9c9aa2c
Daniel <djt52@case.edu>
2018-02-07 11:10:22 -0500

    Added Swagger to ReportingService

--- TypesService/src/main/java/com/revature/caliber/TypesServiceApplication.java
+++ TypesService/src/main/java/com/revature/caliber/TypesServiceApplication.java
@@ -3,7 +3,13 @@ package com.revature.caliber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+@EnableSwagger2
 @SpringBootApplication
 @EnableEurekaClient
 public class TypesServiceApplication {
@@ -11,4 +17,12 @@ public class TypesServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TypesServiceApplication.class, args);
 	}
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.revature.caliber.controller"))              
+          .paths(PathSelectors.any())                          
+          .build();
+    }
 }
