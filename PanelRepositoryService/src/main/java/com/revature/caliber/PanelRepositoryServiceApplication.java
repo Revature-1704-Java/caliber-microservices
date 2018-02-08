47718da083fcd6b5739dcfcbea2ccfc862ea1ba0
JackMV <jack.mccabev@gmail.com>
2018-02-08 13:19:26 -0500

    Added cloud config to panel

--- PanelRepositoryService/src/main/java/com/revature/caliber/PanelRepositoryServiceApplication.java
+++ PanelRepositoryService/src/main/java/com/revature/caliber/PanelRepositoryServiceApplication.java
@@ -1,12 +1,7 @@
 package com.revature.caliber;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.revature.caliber.model.SimplePanel;
-import com.revature.caliber.repository.PanelRepository;
 
 @SpringBootApplication
 public class PanelRepositoryServiceApplication {
