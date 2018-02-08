1c557fbd334e8333b63ee164c2d1720959027a54
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-02-08 10:07:41 -0500

    Added Zuul Service routes in allFactory and vpFactory
    Category
       allFactory - all.getAllCategories
       vpFactory - vp.getAllCategories
       vpFactory - vp.updateCategorie
       vpFactory - vp.saveCategory
    
    Save and getAll both hit the same endpoint... one post, one get seems odd to me.

--- PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/PanelFeedbackRepositoryServiceApplication.java
+++ PanelFeedbackRepositoryService/src/main/java/com/revature/caliber/PanelFeedbackRepositoryServiceApplication.java
@@ -2,6 +2,7 @@ package com.revature.caliber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
