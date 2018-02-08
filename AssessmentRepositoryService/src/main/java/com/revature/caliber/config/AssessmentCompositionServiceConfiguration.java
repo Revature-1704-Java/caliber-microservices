23a2a8540a917e9f7ad92b09a56a3f894a5f70cb
Matthew McCormick <matthew.o.mccormick@gmail.com>
2018-02-07 20:05:36 -0500

    Squashing warnings

--- AssessmentRepositoryService/src/main/java/com/revature/caliber/config/AssessmentCompositionServiceConfiguration.java
+++ AssessmentRepositoryService/src/main/java/com/revature/caliber/config/AssessmentCompositionServiceConfiguration.java
@@ -4,11 +4,9 @@ import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.revature.caliber.model.Assessment;
 import com.revature.caliber.service.AssessmentCompositionService;
 
 @Configuration
--- AssessmentRepositoryService/src/main/java/com/revature/caliber/model/SimpleNote.java
+++ AssessmentRepositoryService/src/main/java/com/revature/caliber/model/SimpleNote.java
@@ -2,20 +2,6 @@ package com.revature.caliber.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Length;
-
 public class SimpleNote implements Serializable {
 	private static final long serialVersionUID = 7785756076682011103L;
 
