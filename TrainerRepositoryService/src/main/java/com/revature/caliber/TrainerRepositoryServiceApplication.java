171462bb315619271dc005f9d03643d92723bd1f
Emmanuel <emmanuelgeorge93@gmail.com>
2018-02-08 11:35:20 -0500

    added eureka zuul config

--- TrainerRepositoryService/src/main/java/com/revature/caliber/TrainerRepositoryServiceApplication.java
+++ TrainerRepositoryService/src/main/java/com/revature/caliber/TrainerRepositoryServiceApplication.java
@@ -2,7 +2,11 @@ package com.revature.caliber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+
+@EnableEurekaClient
 @SpringBootApplication
 public class TrainerRepositoryServiceApplication {
 	public static void main(String[] args) {
